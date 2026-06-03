3da177569deccb5476be239e3fe405d44cb81659
Krishna Yadav <krishnayadav07773@gmail.com>
2026-06-03 18:32:55 +0530

    Number Logic

---
+++ number-logic/palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+ int main(){
+    char str[50], rev[50];
+
+    printf("enter a string");
+    scanf("%s", str);
+
+    strcpy(rev, str);
+    strrev(rev);
+
+    if(strcmp(str, rev)==0){
+     printf("palindrome");
+    }else{
+         printf("not palindrome");
+    }
+
+    return 0;
+ }
